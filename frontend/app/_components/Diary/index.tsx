28aef6a3f6c940527403362267c4ec6438b206b8
chihaso <cycyccq@gmail.com>
2023-12-04 15:01:22 +0900

    rootにアクセスした時、その日の記録を　を表示
    - ダミーデータを置く
    - ひとまず日付と記録ブロックの名前だけ表示

---
+++ frontend/app/_components/Diary/index.tsx
@@ -0,0 +1,29 @@
+import { Box, VStack, Heading } from '@chakra-ui/react'
+import { dummyData } from '../../dummyData'
+// import RecordBlock from './_components/RecordBlock'
+
+type Props = {
+  date: Date
+}
+
+const Diary = ({ date }: Props) => {
+  const recordBlocks = dummyData
+  const dateStr = date.toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    weekday: 'short',
+  })
+
+  return (
+    <VStack spacing={4}>
+      <Heading as="h2">{dateStr}</Heading>
+      {recordBlocks.map((recordBlock, index) => (
+        <Box>{recordBlock.name}</Box>
+        // <RecordBlock key={`record-block-${index}`} recordBlock={recordBlock} />
+      ))}
+    </VStack>
+  )
+}
+
+export default Diary
