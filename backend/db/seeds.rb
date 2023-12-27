b7be0ab7034b609b7106cc64ee2c3a2a12e9a3e4
chihaso <cycyccq@gmail.com>
2023-12-27 02:52:32 +0000

    seedデータ追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+diary_block = RecordBlock.find_or_create_by!(name: "日記")
+diary_topic = diary_block.topics.find_or_create_by!({})
+diary_topic.text_contents.find_or_create_by!({})
+
+cat_block = RecordBlock.find_or_create_by!(name: "猫の記録")
+cat_stool_topic = cat_block.topics.find_or_create_by!(name: "うんち")
+cat_stool_topic.multi_level_contents.find_or_create_by!(name: "量", left_label: "少ない", right_label: "多い", size: 10)
+cat_stool_topic.multi_level_contents.find_or_create_by!(name: "毛量", left_label: "少ない", right_label: "多い", size: 4)
+cat_urine_topic = cat_block.topics.find_or_create_by!(name: "おしっこ")
+cat_urine_topic.integer_contents.find_or_create_by!(name: "回数", unit: "回")
